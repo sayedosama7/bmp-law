77e2acefdc1780f7a08281de278526a01b09cf28
sayedosama7 <sayedosama088@gmail.com>
2024-05-23 19:24:09 +0300

    finall

--- js/main.js
+++ js/main.js
@@ -68,4 +68,26 @@ function validateForm() {
     return true;
 }
 
-
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+swiperEl.initialize();
